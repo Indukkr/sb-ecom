ef9e043b4aef4505ff65c54424669e61d9b7fc59
indu <patel.indu02@gmail.com>
2026-02-14 21:10:22 +0530

    Introducing Lombok

--- sb-ecom/src/main/java/com/ecommerce/sb_ecom/model/Category.java
+++ sb-ecom/src/main/java/com/ecommerce/sb_ecom/model/Category.java
@@ -1,8 +1,15 @@
 package com.ecommerce.sb_ecom.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
 @Table(name ="categories")
 public class Category {
 
@@ -11,29 +18,21 @@ public class Category {
     private Long categoryID;
     private String categoryName;
 
-    public Category(Long categoryID, String categoryName) {
-        this.categoryID = categoryID;
-        this.categoryName = categoryName;
-    }
-
-    public Category() {
-    }
-
-    public Long getCategoryID() {
-        return categoryID;
-    }
-
-    public void setCategoryID(Long categoryID) {
-        this.categoryID = categoryID;
-    }
-
-    /*why do we need to use getter and setter , just for experiment try commenting this and check its response over postman and also into H2 database
-    * --> basically without these two function the category name dint get saved and in the database it got saved as null and over postman it dint even appear*/
-    public String getCategoryName() {
-        return categoryName;
-    }
-
-    public void setCategoryName(String categoryName) {
-        this.categoryName = categoryName;
-    }
+//    public Long getCategoryID() {
+//        return categoryID;
+//    }
+//
+//    public void setCategoryID(Long categoryID) {
+//        this.categoryID = categoryID;
+//    }
+//
+//    /*why do we need to use getter and setter , just for experiment try commenting this and check its response over postman and also into H2 database
+//    * --> basically without these two function the category name dint get saved and in the database it got saved as null and over postman it dint even appear*/
+//    public String getCategoryName() {
+//        return categoryName;
+//    }
+//
+//    public void setCategoryName(String categoryName) {
+//        this.categoryName = categoryName;
+//    }
 }
