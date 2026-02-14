c9d082f7d4b57f6415f807b2ff6a8a10eddbc5cc
indu <patel.indu02@gmail.com>
2026-02-14 21:23:05 +0530

    Introducing Lombok

--- sb-ecom/src/main/java/com/ecommerce/sb_ecom/model/Category.java
+++ sb-ecom/src/main/java/com/ecommerce/sb_ecom/model/Category.java
@@ -18,21 +18,10 @@ public class Category {
     private Long categoryID;
     private String categoryName;
 
-//    public Long getCategoryID() {
-//        return categoryID;
-//    }
-//
-//    public void setCategoryID(Long categoryID) {
-//        this.categoryID = categoryID;
-//    }
-//
+
 //    /*why do we need to use getter and setter , just for experiment try commenting this and check its response over postman and also into H2 database
 //    * --> basically without these two function the category name dint get saved and in the database it got saved as null and over postman it dint even appear*/
 //    public String getCategoryName() {
 //        return categoryName;
-//    }
-//
-//    public void setCategoryName(String categoryName) {
-//        this.categoryName = categoryName;
-//    }
+
 }
