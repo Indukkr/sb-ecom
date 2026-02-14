d4a02d40eae996a8b3374a85b322b62f7941be7a
indu <patel.indu02@gmail.com>
2026-02-15 02:56:10 +0530

    Cleaning up Controllers !!

--- sb-ecom/src/main/java/com/ecommerce/sb_ecom/controller/CategoryController.java
+++ sb-ecom/src/main/java/com/ecommerce/sb_ecom/controller/CategoryController.java
@@ -4,12 +4,10 @@ import com.ecommerce.sb_ecom.model.Category;
 import com.ecommerce.sb_ecom.service.CategoryService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -39,27 +37,17 @@ public class CategoryController {
 
     @DeleteMapping("/admin/categories/{categoryID}")
     public ResponseEntity<String> deleteCategory(@PathVariable Long categoryID) {
-        try {
             String status = categoryService.deleteCategory(categoryID);
             return new ResponseEntity<>(status, HttpStatus.OK);
-            // below are multiple way to return
-            //return ResponseEntity.ok(status);
-            //return  ResponseEntity.status(HttpStatus.OK).body(status);
-        } catch (ResponseStatusException e) {
-            return new ResponseEntity<>(e.getReason(), e.getStatusCode());
-        }
     }
 
     //@PutMapping("/api/public/categories/{categoryID}")
     @RequestMapping(value="/public/categories/{categoryID}",method=RequestMethod.PUT)
-    public ResponseEntity<String> updateCategory(@RequestBody Category category,
+    public ResponseEntity<String> updateCategory(@Valid @RequestBody Category category,
                                                  @PathVariable Long categoryID){
-        try{
+
             Category savedCategory = categoryService.updateCategory(category,categoryID);
             return new ResponseEntity<>("categoryID: "+categoryID+" is updated successfully !!", HttpStatus.OK);
-        }catch(ResponseStatusException e){
-            return new ResponseEntity<>(e.getReason(), e.getStatusCode());
-        }
 
     }
 }
--- sb-ecom/src/main/java/com/ecommerce/sb_ecom/service/CategoryServiceImpl.java
+++ sb-ecom/src/main/java/com/ecommerce/sb_ecom/service/CategoryServiceImpl.java
@@ -5,23 +5,17 @@ import com.ecommerce.sb_ecom.exception.ResourceNotFoundException;
 import com.ecommerce.sb_ecom.model.Category;
 import com.ecommerce.sb_ecom.repositories.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Service;
-import org.springframework.web.server.ResponseStatusException;
 
-import java.util.ArrayList;
+import org.springframework.stereotype.Service;
+
+
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class CategoryServiceImpl implements CategoryService{
 
-    //private List<Category> categories = new ArrayList<>();
-
     @Autowired
     private CategoryRepository categoryRepository;
-
-    //private Long nextID = 1L;
 
     @Override
     public List<Category> getAllCategories() {
@@ -34,7 +28,6 @@ public class CategoryServiceImpl implements CategoryService{
 
     @Override
     public void createCategory(Category category) {
-        //category.setCategoryID(nextID++
         Category savedCategory = categoryRepository.findByCategoryName(category.getCategoryName());
         if(savedCategory!=null)
             throw new APIException("Category with name "+ category.getCategoryName()+"already Exists");
