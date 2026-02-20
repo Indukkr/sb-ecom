0deef6fa23aac6f7aa95d55de1cadf271a819af4
indu <patel.indu02@gmail.com>
2026-02-20 21:11:37 +0530

    Implementing APIResponse!!

--- sb-ecom/src/main/java/com/ecommerce/sb_ecom/exception/MyGlobalExceptionHandler.java
+++ sb-ecom/src/main/java/com/ecommerce/sb_ecom/exception/MyGlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.ecommerce.sb_ecom.exception;
 
+import com.ecommerce.sb_ecom.payload.APIResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
@@ -28,18 +29,20 @@ public class MyGlobalExceptionHandler {
     }
 
     @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<String> myResponseNotFound(ResourceNotFoundException e){
+    public ResponseEntity<APIResponse> myResponseNotFound(ResourceNotFoundException e){
 
         String message = e.getMessage();
-        return new ResponseEntity<>(message,HttpStatus.NOT_FOUND);
+        APIResponse apiResponse= new APIResponse(message,false);
+        return new ResponseEntity<>(apiResponse,HttpStatus.NOT_FOUND);
 
     }
 
     @ExceptionHandler(APIException.class)
-    public ResponseEntity<String> myAPIException(APIException e){
+    public ResponseEntity<APIResponse> myAPIException(APIException e){
 
         String message = e.getMessage();
-        return new ResponseEntity<>(message,HttpStatus.BAD_REQUEST);
+        APIResponse apiResponse= new APIResponse(message,false);
+        return new ResponseEntity<>(apiResponse,HttpStatus.BAD_REQUEST);
 
     }
 }
