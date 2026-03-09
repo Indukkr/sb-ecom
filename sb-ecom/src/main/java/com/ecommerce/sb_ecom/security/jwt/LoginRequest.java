abee9e08baf764b9f18ea6d9f0e8a10bc829b142
indu <patel.indu02@gmail.com>
2026-03-10 00:11:21 +0530

    Configuring project to work with jwt !!

---
+++ sb-ecom/src/main/java/com/ecommerce/sb_ecom/security/jwt/LoginRequest.java
@@ -0,0 +1,23 @@
+package com.ecommerce.sb_ecom.security.jwt;
+
+public class LoginRequest {
+
+    private String username;
+    private String password;
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return username;
+    }
+
+    public void setName(String username) {
+        this.username = username;
+    }
+}
