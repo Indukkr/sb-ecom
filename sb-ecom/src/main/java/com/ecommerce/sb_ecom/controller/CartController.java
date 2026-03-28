927a74430903b33078a50cf4d4fc5dfe2a5dad78
indu <patel.indu02@gmail.com>
2026-03-28 11:34:58 +0530

    Fetching All Carts !!

--- sb-ecom/src/main/java/com/ecommerce/sb_ecom/controller/CartController.java
+++ sb-ecom/src/main/java/com/ecommerce/sb_ecom/controller/CartController.java
@@ -7,6 +7,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api")
 public class CartController {
@@ -22,6 +24,12 @@ public class CartController {
         CartDTO cartDTO = cartService.addProductToCart(productId,quantity);
 
         return new ResponseEntity<CartDTO>(cartDTO, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/carts")
+    public ResponseEntity<List<CartDTO>> getCarts(){
+        List<CartDTO> cartDTOS = cartService.getAllCarts();
+        return new ResponseEntity<List<CartDTO>>(cartDTOS,HttpStatus.FOUND);
     }
 
 }
