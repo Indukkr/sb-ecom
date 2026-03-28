5596d791a604439dc1210d05b3f9ca36e797eb63
indu <patel.indu02@gmail.com>
2026-03-28 12:50:30 +0530

    Fetching Users Cart !!

--- sb-ecom/src/main/java/com/ecommerce/sb_ecom/service/CartServiceImpl.java
+++ sb-ecom/src/main/java/com/ecommerce/sb_ecom/service/CartServiceImpl.java
@@ -27,6 +27,9 @@ public class CartServiceImpl implements CartService{
     CartRepository cartRepository;
 
     @Autowired
+    AuthUtil authUtil;
+
+    @Autowired
     ProductRepository productRepository;
 
     @Autowired
@@ -34,9 +37,6 @@ public class CartServiceImpl implements CartService{
 
     @Autowired
     ModelMapper modelMapper ;
-
-    @Autowired
-    AuthUtil authUtil;
 
     @Override
     public CartDTO addProductToCart(Long productId, Integer quantity) {
@@ -120,6 +120,23 @@ public class CartServiceImpl implements CartService{
 
                 }).collect(Collectors.toList());
         return cartDTOS;
+    }
+
+    @Override
+    public CartDTO getCart(String emailId, Long cartId) {
+        Cart cart = cartRepository.findCartByEmailIdAndCartId(emailId,cartId);
+
+        if(cart == null){
+            throw new ResourceNotFoundException("Cart","cartId",cartId);
+        }
+        CartDTO cartDTO = modelMapper.map(cart,CartDTO.class);
+        cart.getCartItems().forEach(c->
+                c.getProduct().setQuantity(c.getQuantity()));
+        List<ProductDTO> products = cart.getCartItems().stream()
+                .map(p->modelMapper.map(p.getProduct(),ProductDTO.class))
+                .toList();
+        cartDTO.setProducts(products);
+        return cartDTO;
     }
 
     private Cart createCart(){
